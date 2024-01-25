8864ee0fccc280c32b7b4efb95f8020248a9eadd
Andrew Sukach <andrewsukach@gmail.com>
2024-01-25 15:19:35 -0500

    token type enum

---
+++ token_type.h
@@ -0,0 +1,29 @@
+#pragma once
+
+enum struct token_type {
+  l_paren__,
+  r_paren__,
+  l_brace__,
+  r_brace__,
+  dot__,
+  semi__,
+  comma__,
+  plus__,
+  minus__,
+  star__,
+  slash__,
+  bang__,
+  equal__,
+  plusequal__,
+  minusequal__,
+  starequal__,
+  slashequal__,
+  bangequal__,
+  equalequal__,
+  true__,
+  false__,
+  nil__,
+  string__,
+  number__,
+  identifier__
+};
